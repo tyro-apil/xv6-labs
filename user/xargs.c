1bdb73f44b0d5d6ceeb1ad226200a227be7d93f1
tyro-apil <078bct017.apil@pcampus.edu.np>
2025-02-27 17:10:40 +0545

    feat: xargs completed->multi-line

--- user/xargs.c
+++ user/xargs.c
@@ -2,73 +2,81 @@
 #include "kernel/stat.h"
 #include "kernel/fcntl.h"
 #include "user/user.h"
-
 #include "kernel/param.h"
 
-int
-main(int argc, char *argv[])
-{   
-    char *buf[MAXARG];
-
-    int count=0;
-
-    for(int i=1; i<argc; ++i){
-        buf[i-1]=argv[i];
-        count++;
-    }
-
-    char line[512], c;
-    int j=0;
-
-    while(read(0, &c, sizeof(char))==sizeof(char)){
-        
-        if(c == '\n'){
-            // printf("new line\n");
-            line[j]='\0';
-
-            // allocate memory 
-            buf[count] = (char *)malloc(j+1);
-            
-            strcpy(buf[count], line);
-            count++;
-            j=0;
-        }
-        else{
-
-            line[j++]=c;
-        }
-    }
-    // buf[count] = (char *)malloc(j+1);
-    buf[count] = 0;
-    // printf("arg count: %d\n", count);
-
-    // Print all arguments as debug purpose
-    // for(int i=0; i<count; ++i){
-    //     printf("%s\n",buf[i]);
-    // }
-
-    // All commands are in root path
-    char *cmd_path=(char *)malloc(strlen(buf[0])+2);
-    *cmd_path='/';      // Be aware of ""->char* and ''->char
-    strcpy(cmd_path+1, buf[0]);
-
-    // See command full path
-    // printf("%s\n", cmd_path);
-
-
-    int pid=fork();
-
-    if(pid<0){
-        fprintf(2, "Fork error\n");
+int main(int argc, char *argv[])
+{
+    if(argc < 2) {
+        fprintf(2, "Usage: xargs command [args...]\n");
         exit(1);
     }
-    else if(pid>0){
-        wait(0);
+    
+    char *base_args[MAXARG];
+    int base_count = 0;
+    
+    for(int i=1; i<argc; i++) {
+        base_args[base_count] = argv[i];
+        base_count++;
     }
-    else{
-        exec(cmd_path, buf);
-    }
+    
+    char line[512];
+    char c;
+    int j = 0;
+    
+    // Process each line of input separately
+    while(read(0, &c, sizeof(char)) == sizeof(char)) {
+        if(c == '\n') {
+            line[j] = '\0';
+            
+            char *exec_args[MAXARG];
+            int arg_count = 0;
+            
+            for(int i=0; i<base_count; i++) {
+                exec_args[arg_count++] = base_args[i];
+            }
+            
+
+            // Add this line as an argument
+            exec_args[arg_count] = (char *)malloc(j+1);
+            strcpy(exec_args[arg_count], line);
+            arg_count++;
+            
+            exec_args[arg_count] = 0;
+
+            
+            // Create the command path
+            char cmd_path[MAXARG];
+            cmd_path[0] = '/';
+            strcpy(cmd_path+1, exec_args[0]);
+            
 
 
-    return 0;
+            int pid = fork();
+            if(pid < 0) {
+                fprintf(2, "Fork error\n");
+                exit(1);
+            }
+            else if(pid == 0) {
+                exec(cmd_path, exec_args);
+                fprintf(2, "exec failed\n");
+                exit(1);
+            }
+            else {
+                wait(0);
+                
+                // Free the allocated memory
+                if(j > 0) {
+                    free(exec_args[base_count]);
+                }
+            }
+            
+            // Reset for the next line
+            j = 0;
+        }
+        else {
+            line[j++] = c;
+        }
+    }
+    
+    exit(0);
 }
